06bc2d9cfb3fae798b604682020787926966478f
alvvolobuev <alvvolobuev@gmail.com>
2020-12-26 11:27:40 +0300

    Реализовано задание b1_binary_search.py

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -1,13 +1,21 @@
-from typing import Sequence, Optional
+# Волобуев Александр
 
+from typing import Sequence, Optional
 
 def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     """
-    Performs binary search of given element inside of array
+    Performs binary search of given element inside of array/
+    Выполняет двоичный поиск заданного элемента внутри массива
 
-    :param elem: element to be found
-    :param arr: array where element is to be found
-    :return: Index of element if it's presented in the arr, None otherwise
+    :param elem: element to be found/
+    элемент, который нужно найти
+    :param arr: array where element is to be found/
+    массив, в котором должен быть найден элемент
+    :return: Index of element if it's presented in the arr, None otherwise/
+    Индекс элемента, если он представлен в arr, не иначе
     """
-    print(elem, arr)
+
+    for i in arr:
+        if i == elem:
+           return arr.index(i)
     return None
