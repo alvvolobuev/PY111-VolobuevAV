25fbeafe4b9dc433a84a241ac17f838ab99e7aff
alvvolobuev <alvvolobuev@gmail.com>
2020-12-28 18:29:47 +0300

    Переделанное задание b1

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -2,6 +2,7 @@
 
 from typing import Sequence, Optional
 
+    
 def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     """
     Performs binary search of given element inside of array/
@@ -15,7 +16,26 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     Индекс элемента, если он представлен в arr, не иначе
     """
 
-    for i in arr:
-        if i == elem:
-           return arr.index(i)
-    return None
+    if not arr:
+        return None
+
+    middle = len(arr) // 2
+    min_my = 0
+    max_my = len(arr) - 1
+
+    # You should return first occurrence from the array. 2 != 1
+    # Вы должны вернуть первое вхождение из массива. 2 != 1
+
+    while arr[middle] != elem and min_my <= max_my and middle != 1:
+        if elem > arr[middle]:
+            min_my = middle + 1
+        else:
+            max_my = middle - 1
+        middle = (min_my + max_my) // 2
+
+    if max_my < min_my:
+        return None
+    elif not middle % 2:
+        middle //= 2
+    else:
+        return middle
